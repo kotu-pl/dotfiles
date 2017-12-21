2f780f8a149a939c6b6a80e877fcfa9db472f5e4
Marcin Kot <marcin@kotu.pl>
2017-12-21 15:04:48 +0100

    REfactor ZSH tweaks

--- zshrc.d/tweaks.zsh
+++ zshrc.d/tweaks.zsh
@@ -1,2 +1,56 @@
- setopt  NO_NOMATCH                                                              
- unset PAGER 
+# General
+## make it more responsive
+export KEYTIMEOUT=1
+export VISUAL=vim
+export EDITOR=$VISUAL
+export TERM=xterm-256color
+
+# VIM mode 
+bindkey -v
+
+## Make ctrl + s work                                                            
+vim()
+{
+    local STTYOPTS="$(stty --save)"
+    stty stop '' -ixoff
+    command vim "$@"
+    stty "$STTYOPTS"
+}
+
+bindkey '^P' up-history
+bindkey '^N' down-history
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+bindkey '^r' history-incremental-search-backward
+
+function zle-line-init zle-keymap-select {
+    VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
+    RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/}$(git_custom_status) $EPS1"
+    zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+# b to go back one word
+# 2b to go back two words
+# dw to delete a word
+# dd to delete the entire line
+# d$ to delete from the current cursor position to the end of the line
+# d0 to delete from the current cursor position to the beginning of the line
+# w to go forward one word, and so forth
+
+# History
+
+HISTFILE=~/.histfile                                                            
+HISTSIZE=10000000                                                                   
+SAVEHIST=10000000
+setopt share_history                                                          
+setopt hist_ignore_all_dups                                                     
+setopt hist_ignore_space
+
+# Tweaks 
+## git issue fix (zsh: no matches found: HEAD^)
+setopt NO_NOMATCH                                                              
+unset PAGER 
